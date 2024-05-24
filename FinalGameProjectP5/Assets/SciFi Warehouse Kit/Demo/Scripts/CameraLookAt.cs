a38178bc24910afed1701be79cd7a3a8c77f6204
MrLongarms <704718@student.dusd.net>
2024-05-24 13:53:28 -0700

    Getting There.
    We are taking some steps back on the script and change the camera angle.

--- FinalGameProjectP5/Assets/SciFi Warehouse Kit/Demo/Scripts/CameraLookAt.cs
+++ FinalGameProjectP5/Assets/SciFi Warehouse Kit/Demo/Scripts/CameraLookAt.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBot : MonoBehaviour
+public class CameraLookAt : MonoBehaviour
 {
+    public Transform target;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class EnemyBot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(target);
     }
 }
--- FinalGameProjectP5/Assets/SciFi Warehouse Kit/Demo/Scripts/PickUpController.cs
+++ FinalGameProjectP5/Assets/SciFi Warehouse Kit/Demo/Scripts/PickUpController.cs
@@ -4,77 +4,30 @@ using UnityEngine;
 
 public class PickUpController : MonoBehaviour
 {
-    [Header("Pickup Setting")]
-    [SerializeField] Transform holdArea;
-    private GameObject heldObj;
-    private Rigidbody heldObjRB;
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
 
-    [Header("Physics Parameters")]
-    [SerializeField] private float pickupRange = 5.0f;
-    [SerializeField] private float pickupForce = 150.0f;
+    private Rigidbody rb;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if(Input.GetKey(KeyCode.Space))
-        {
-            if(heldObj != null)
-            {
-                RaycastHit hit;
-                if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickupRange))
-                {
-                    PickupObject(hit.transform.gameObject);
-                }
-            }
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 
-            else
-            {
-                DropObject();
-            }
-        }
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
 
-        if (heldObj != null)
-        {
-            MoveObject();
-        }
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
     }
-    
-    void MoveObject()
-    {
-        if(Vector3.Distance(heldObj.transform.position, holdArea.position) > 0.1f)
-        {
-            Vector3 moveDirection = (holdArea.position - heldObj.transform.position);
-            heldObjRB.AddForce(moveDirection * pickupForce);
-        }
-    }
-    void PickupObject(GameObject pickObj)
-    {
-        if (pickObj.GetComponent<Rigidbody>())
-        {
-            heldObjRB = pickObj.GetComponent<Rigidbody>();
-            heldObjRB.useGravity = false;
-            heldObjRB.drag = 10;
-            heldObjRB.constraints = RigidbodyConstraints.FreezeRotation;
-
-            heldObjRB.transform.parent = holdArea;
-            heldObj = pickObj;
-        }
-    }
-
-    void DropObject()
-    {
-            heldObjRB.useGravity = true;
-            heldObjRB.drag = 1;
-            heldObjRB.constraints = RigidbodyConstraints.None;
-
-            heldObj.transform.parent = null;
-            heldObj = null;
-    }
-
-    
 }
